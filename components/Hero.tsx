79e6c83bb63693d23ec96011629ab106cacce3e3
Erica Sanchez <ericazach@gmail.com>
2024-10-03 14:18:37 +0200

    improve Hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,27 +1,42 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col h-full  md:flex-row justify-center items-center mx-4 md:mx-8 lg:mx-16">
-      <div className="h-1/2 w-full md:w-full relative md:h-full ">
+    <div className="flex flex-col h-full md:flex-row justify-center items-center md:mx-8 lg:mx-16">
+      {/* IMAGE CONTAINER */}
+      <div className="h-1/2 w-full md:w-full md:h-full relative ">
         <Image
           src="/girl.png"
           alt="Erica's Profile"
           fill
-          className="object-contain px-4 md:px-8 lg:px-16"
+          className="object-contain  "
         />
       </div>
-      <div className="h-1/2 w-full md:w-full md:h-full flex-col flex justify-center items-center">
-        <h1 className="text-5xl font-bold text-center text-white">
-          Erica Portfolio
+      {/* TEXT CONTAINER */}
+      <div className="h-1/2 w-full md:w-full md:h-full flex-col flex justify-center px-5 ">
+        <h1 className="text-4xl my-2 md:text-5xl lg:text-6xl font-bold text-white">
+          Turning Ideas into <span className="text-[#DD798F]">Code Magic</span>
         </h1>
-        <p className="text-2xl text-center text-white">
-          Modern & Minimalist Portfolio Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Reiciendis ut, iste dolor, consequuntur eligendi
-          natus distinctio totam similique et saepe incidunt sed expedita maxime
-          sequi eaque aut qui dolorem quidem?
+        <p className="md:text-lg text-white ">
+          Welcome to my portfolio! I’m a software developer with a passion for
+          crafting efficient, creative solutions using React and Java. My
+          mission is to turn complex ideas into user-friendly apps that make an
+          impact. Let’s build something amazing together!
         </p>
+        <div className="flex gap-4 my-6">
+          <Link href="/about">
+            <button className="text-white bg-[#29727C] hover:bg-[#DD798F] rounded-md px-4 py-2">
+              Contact me!
+            </button>
+          </Link>
+          <Link href="/work">
+            <button className="text-white bg-[#DD798F] hover:bg-[#29727C] rounded-md px-4 py-2 ">
+              My Work
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
